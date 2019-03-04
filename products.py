fa4ff9da10c7c845bfcb07dc15a4280e0faa167e
Taquito Slayer <yaboy@yaboy.com>
2019-03-04 10:11:02 -0700

    Removed cfscrape cause instance not working

--- products.py
+++ products.py
@@ -1,11 +1,9 @@
 import requests
 import re
 import json
-import cfscrape
 import bs4 as bs
 
-L = requests.session()
-r = cfscrape.create_scraper(sess=L)
+r = requests.session()
 
 headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4','referer': 'https://www.supremenewyork.com/mobile/'}
 
